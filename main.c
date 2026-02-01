781ce7105993c16a5f43f8b64ab01f98093a7caf
Eugene Bakin <bakin.eugene@ya.ru>
2026-02-01 23:31:59 +0500

    FastPWM is working

--- main.c
+++ main.c
@@ -60,12 +60,12 @@ ISR(ADC_vect) {
 #define KEY_4     45
 #define KEY_5     90
 
-volatile uint8_t menu[6] = {0, 1, 2, 3, 4, 5};
+volatile uint8_t menu[6] = {0, 3, 6, 9, 12, 15};
 volatile uint8_t menu_counter = 0;
 
 static void menu_key(uint8_t v) {
     if (v < KEY_1) {
-        if (menu[menu_counter] < 8) ++menu[menu_counter];
+        if (menu[menu_counter] < 24) ++menu[menu_counter];
     } else if (v < KEY_2) {
         if (menu_counter < 5) ++menu_counter;
     } else if (v < KEY_3) {
@@ -75,11 +75,17 @@ static void menu_key(uint8_t v) {
     } else if (v < KEY_5) {
 
     }
+
+    if (menu_counter == 0) {
+        TIMER_0_TOP_A_VALUE(255 - 10 * menu[0] - 15);
+    }
 }
 
 int main(void) {
     PORTB_SET_INPUT(PB4);
     PORTB_SET_HIGH(PB4);
+    PORTB_SET_OUTPUT(PB0);
+    PORTB_SET_HIGH(PB0);
 
     DDRB |= 1 << 2;
 
@@ -97,16 +103,30 @@ int main(void) {
     ADC_ENABLE_INTERRUPT();
     ADC_START();
 
+    TIMER_0_TOP_A_VALUE(255 - 10 * menu[0] - 15);
+    TIMER_0_FAST_PWM_MODE_CLEAR_SET();
+    TIMER_0_FAST_PWM_TOP_MODE_ENABLE();
+    TIMER_0_64_PRESCALE();
+
     sei();
 
     while (1) {
         Color current_color;
         pgm_read_block(&colors[menu_counter], (void*)&current_color, sizeof(Color));
 
+        uint8_t index = menu[menu_counter] / 3;
+        uint8_t reminder = menu[menu_counter] % 3;
+
         for (int i = 0; i < 8; ++i) {
-            write<2>(menu[menu_counter] <= i ? current_color.g : 0);
-            write<2>(menu[menu_counter] <= i ? current_color.r : 0);
-            write<2>(menu[menu_counter] <= i ? current_color.b : 0);
+            uint8_t m = 3;
+            if (i == index) {
+                m = 3 - reminder;
+            } else if (i < index) {
+                m = 0;
+            }
+            write<2>(current_color.g * m);
+            write<2>(current_color.r * m);
+            write<2>(current_color.b * m);
         }
         _delay_ms(100);
     }
